c060b3e54b39dda04df5d39ec38c6564591b1350
subha0341 <105307520+subha0341@users.noreply.github.com>
2022-06-29 23:32:13 +0530

    Update application.py

--- application.py
+++ application.py
@@ -14,4 +14,4 @@ def index():
 	return render_template('index.html', blogs = blogs)
 
 if(__name__ == "__main__"):
-	application.run(host="0.0.0.0", port=5002, debug=True)
+	application.run(host="0.0.0.0", port=5000, debug=True)
